a1186850b89f36a6df44328ea67a416868b05640
Jacob Hummer <jcbhmr@outlook.com>
2024-05-23 22:04:58 +0000

    test things

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,30 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const HeaderListItem = struct {
+    name: []const u8,
+    value: []const u8,
+};
+
+pub const Headers = struct {
+    allocator: std.mem.Allocator,
+    headerList: std.ArrayList(HeaderListItem),
+
+    pub fn append(h: *Headers, name: []const u8, value: []const u8) void {
+        h.headerList.append(HeaderListItem{ .name = name, .value = value }) catch @panic("Allocator.Error");
+    }
+};
+
+test "Headers append()" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var headers = Headers{
+        .allocator = allocator,
+        .headerList = std.ArrayList(HeaderListItem).init(allocator),
+    };
+    headers.append("Content-Type", "text/html; charset=UTF-8");
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+export fn fetch(a: i32, b: i32) i32 {
+    return a + b;
 }
